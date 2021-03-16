be18c14cf7e9e9d3f61d370d32b0b99a6f4aa1d0
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-03-16 11:10:13 -0300

    Alter theoretically together.

---
+++ src/Form/MigrationExecuteForm.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\dgi_migrate\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+
+use Drupal\migrate\MigrateMessage;
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\migrate_tools\Form\MigrationExecuteForm as MigrationExecuteFormBase;
+
+class MigrationExecuteForm extends MigrationExecuteFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $operation = $form_state->getValue('operation');
+
+    if ($form_state->getValue('limit')) {
+      $limit = $form_state->getValue('limit');
+    }
+    else {
+      $limit = 0;
+    }
+
+    if ($form_state->getValue('update')) {
+      $update = $form_state->getValue('update');
+    }
+    else {
+      $update = 0;
+    }
+    if ($form_state->getValue('force')) {
+      $force = $form_state->getValue('force');
+    }
+    else {
+      $force = 0;
+    }
+
+    $migration = $this->getRouteMatch()->getParameter('migration');
+    if ($migration) {
+      /** @var \Drupal\migrate\Plugin\MigrationInterface $migration_plugin */
+      $migration_plugin = $this->migrationPluginManager->createInstance($migration->id(), $migration->toArray());
+      $migrateMessage = new MigrateMessage();
+
+      switch ($operation) {
+        case 'import':
+
+          $options = [
+            'limit' => $limit,
+            'update' => $update,
+            'force' => $force,
+          ];
+
+          $executable = new MigrateBatchExecutable($migration_plugin, $migrateMessage, $options);
+          batch_set($executable->prepareBatch());
+
+          break;
+
+        default:
+
+          parent::submitForm($form, $form_state);
+
+          break;
+
+      }
+    }
+  }
+
+
+}
---
+++ src/Routing/RouteSubscriber.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\dgi_migrate\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+use Drupal\dgi_migrate\Form\MigrationExecuteForm;
+
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('migrate_tools.execute')) {
+      $route->setDefault('_form', MigrationExecuteForm::class);
+    }
+  }
+
+}
