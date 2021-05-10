e210dfc4c2de2a586f0d8917fb99b73381c49292
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-05-10 13:41:18 -0300

    Better checking and reporting on existence in our set of allowed values for note types.

--- src/Plugin/migrate/process/StaticMap.php
+++ src/Plugin/migrate/process/StaticMap.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\dgi_migrate\Plugin\migrate\process;
 
+use Drupal\migrate\MigrateSkipProcessException;
+use Drupal\migrate\MigrateSkipRowException;
+use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Plugin\migrate\process\StaticMap as Upstream;
 
 /**
@@ -26,9 +29,33 @@ class StaticMap extends Upstream {
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition) {
     // Remap our array of two-tuples onto the map structure.
-    $configuration['map'] = array_column($configuration['map'], 1, 0);
+    if (!($configuration['original_map_structure'] ?? FALSE)) {
+      $configuration['map'] = array_column($configuration['map'], 1, 0);
+    }
 
     parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    try {
+      return parent::transform($value, $migration_executable, $row, $destination_property);
+    }
+    catch (MigrateSkipRowException $e) {
+      if ($this->configuration['skip_process_instead_of_row'] ?? FALSE) {
+        $message = strtr('Could not map ":value" when processing :property; aborting processing.', [
+          ':value' => $value,
+          ':property' => $destination_property,
+        ]);
+        $migration_executable->saveMessage($message, MigrationInterface::MESSAGE_WARNING);
+        throw new MigrateSkipProcessException($message, 0, $e);
+      }
+      else {
+        throw $e;
+      }
+    }
   }
 
 }
