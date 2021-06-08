57396e4a983f7531524e10ee9effad6d87cbb969
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-06-08 15:10:01 -0300

    Fix issue with the batch failing to exit appropriately.
    ... derped, using null coalescing on something that would never be null...

---
+++ src/MigrateBatchException.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\dgi_migrate;
+
+/**
+ * Batch exception typification.
+ */
+class MigrateBatchException extends \Exception {
+
+  /**
+   * Store the "finished" value, if we want to provide it.
+   *
+   * @var float|null
+   */
+  protected $finished;
+
+  /**
+   * Constructor.
+   */
+  public function __construct($message, $finished = NULL, $previous = NULL) {
+    parent::__construct($message, NULL, $previous);
+
+    $this->finished = $finished;
+  }
+
+  /**
+   * Get the stored "finished" value.
+   *
+   * @return float|null
+   *   The finished value, if provided; otherwise, NULL... which means that it
+   *   is up to the handler to calculate.
+   */
+  public function getFinished() {
+    return $this->finished;
+  }
+
+}
