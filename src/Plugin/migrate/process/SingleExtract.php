9d70f18be80c737bd7597b34ceea143e186d4833
Adam Vessey <adam-vessey@users.noreply.github.com>
2021-08-17 12:52:42 -0300

    Add something of a comment to describe the silliness.

--- src/Plugin/migrate/process/SingleExtract.php
+++ src/Plugin/migrate/process/SingleExtract.php
@@ -7,6 +7,10 @@ use Drupal\migrate\Plugin\migrate\process\Extract;
 /**
  * Extract from a single value.
  *
+ * Identical to the core "extract" plugin, except avoiding the "handle_multiple"
+ * bit of the annotation, 'cause we want to handle individual arrays in a bigger
+ * array.
+ *
  * @MigrateProcessPlugin(
  *   id = "dgi_migrate.process.single_extract",
  * )
