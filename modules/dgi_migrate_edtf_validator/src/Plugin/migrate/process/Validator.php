696b01e5ac8fae0b518a6cf977a18aa8185f2f3a
JojoVes <JojoVes@users.noreply.github.com>
2021-11-26 17:56:02 -0400

    simplify logic

--- modules/dgi_migrate_edtf_validator/src/Plugin/migrate/process/Validator.php
+++ modules/dgi_migrate_edtf_validator/src/Plugin/migrate/process/Validator.php
@@ -60,9 +60,7 @@ class Validator extends ProcessPluginBase implements ConfigurableInterface {
    * {@inheritdoc}
    */
   public function setConfiguration(array $configuration) {
-    foreach (['intervals', 'sets', 'strict'] as $key) {
-      $this->configuration[$key] = (isset($configuration[$key]) ? $configuration[$key] : $this->defaultConfiguration()[$key]);
-    }
+    $this->configuration = $configuration + $this->defaultConfiguration();
   }
 
   /**
