dbd6c63a0ea5c43f5acb433d2b8992f4d01e45f2
Adam Vessey <adam-vessey@users.noreply.github.com>
2022-02-22 17:23:44 -0400

    Coding standards?

--- src/Plugin/migrate/process/ParseFoxml.php
+++ src/Plugin/migrate/process/ParseFoxml.php
@@ -4,15 +4,6 @@ namespace Drupal\dgi_migrate\Plugin\migrate\process;
 
 use Drupal\foxml\Plugin\migrate\process\Parse;
 use Drupal\foxml\Utility\Fedora3\FoxmlParser;
-use Drupal\foxml\Utility\Fedora3\Element\DigitalObject;
-use Drupal\migrate\ProcessPluginBase;
-use Drupal\migrate\MigrateExecutableInterface;
-use Drupal\migrate\Row;
-use Drupal\migrate\MigrateException;
-
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Parse FOXML.
@@ -21,15 +12,16 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   id = "dgi_migrate.parse_foxml"
  * )
  *
- * @deprecated with the pulling out of things to the "foxml" module.
+ * @deprecated in %deprecation-version% and is removed from %removal-version%
+ * @see https://github.com/discoverygarden/dgi_migrate/pull/21
  */
-class ParseFoxml extends Parse implements ContainerFactoryPluginInterface {
+class ParseFoxml extends Parse {
 
   /**
    * Constructor.
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, FoxmlParser $parser) {
-    @trigger_error('\Drupal\dgi_migrate\Plugin\migrate\process\ParseFoxml has been deprected; use \Drupal\foxml\Plugin\migrate\process\Parse instead.', E_USER_DEPRECATED);
+    @trigger_error('\Drupal\dgi_migrate\Plugin\migrate\process\ParseFoxml is deprecated in %deprecation-version%; use \Drupal\foxml\Plugin\migrate\process\Parse before its removal in %removal-version%.', E_USER_DEPRECATED);
     parent::__construct($configuration, $plugin_id, $plugin_definition, $parser);
   }
 
