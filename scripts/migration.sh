5a5a5cc2fcb8518025566f0ec49d22b1956fc60e
Adam Vessey <adam-vessey@users.noreply.github.com>
2022-04-05 16:45:53 -0300

    Reorganize things.

--- scripts/migration.sh
+++ scripts/migration.sh
@@ -6,18 +6,4 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 init_vars ${1:-/opt/staging/dgi_migrate}
 
-set -x
-declare NUM=$(get_op_number)
-set +x
-
-{
-  set -x
-  # The base import
-  wwwdo $TIME --verbose $DRUSH dgi-migrate:import "--root=$DRUPAL_ROOT" "--uri=$URI" "--user=$DRUPAL_USER" "--group=$MIGRATION_GROUP" "${@:2}" |& wwwdo tee "$LOG_DIR/$NUM-import.log" > /dev/null
-  wwwdo mkdir -p "$LOG_DIR/$NUM-messages"
-  wwwdo $DRUSH migrate:status --root=$DRUPAL_ROOT --uri=$URI --group=$MIGRATION_GROUP --field=id --format=string | \
-  while read NAME ; do
-    wwwdo $DRUSH migrate:messages --root=$DRUPAL_ROOT --uri=$URI --format=json $NAME | wwwdo tee "$LOG_DIR/$NUM-messages/$NAME.json" > /dev/null
-  done
-  set +x
-} |& wwwdo tee "$LOG_DIR/$NUM-run.log"
+do_migration $(get_op_number) ${@:2}
--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -6,13 +6,4 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 init_vars ${1:-/opt/staging/dgi_migrate}
 
-set -x
-declare NUM=$(get_op_number)
-set +x
-
-{
-  set -x
-  # The base rollback.
-  wwwdo $TIME --verbose $DRUSH dgi-migrate:rollback --root=$DRUPAL_ROOT --uri=$URI --user=$DRUPAL_USER --group=$MIGRATION_GROUP "${@:2}" |& wwwdo tee "$LOG_DIR/$NUM-rollback.log" > /dev/null
-  set +x
-} |& wwwdo tee "$LOG_DIR/$NUM-run.log"
+do_rollback $(get_op_number) ${@:2}
