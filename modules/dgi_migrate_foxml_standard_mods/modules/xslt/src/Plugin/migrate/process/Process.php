3e66c0e8588b2910e85e31612658a05b8f31660c
Adam Vessey <adam-vessey@users.noreply.github.com>
2022-07-05 12:31:07 -0300

    Add in the missing include.

--- modules/dgi_migrate_foxml_standard_mods/modules/xslt/src/Plugin/migrate/process/Process.php
+++ modules/dgi_migrate_foxml_standard_mods/modules/xslt/src/Plugin/migrate/process/Process.php
@@ -5,6 +5,8 @@ namespace Drupal\dgi_migrate_foxml_standard_mods_xslt\Plugin\migrate\process;
 use Drupal\dgi_saxon_helper_migrate\Plugin\migrate\process\Saxon;
 use Drupal\dgi_saxon_helper\TransformerInterface;
 
+use Drupal\Core\Url;
+
 /**
  * Wrapper to help perform _our_ transformation.
  *
