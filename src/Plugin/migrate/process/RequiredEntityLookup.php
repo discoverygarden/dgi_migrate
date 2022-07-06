bd25fcbfbc0f3b0c93f82656cd56436b2b7388d2
Adam Vessey <adam-vessey@users.noreply.github.com>
2022-07-06 17:37:42 -0300

    Move to use the missing_behavior helper.

--- src/Plugin/migrate/process/RequiredEntityLookup.php
+++ src/Plugin/migrate/process/RequiredEntityLookup.php
@@ -2,13 +2,19 @@
 
 namespace Drupal\dgi_migrate\Plugin\migrate\process;
 
-use Drupal\migrate\MigrateSkipRowException;
 use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Row;
 use Drupal\migrate_plus\Plugin\migrate\process\EntityLookup;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Stronger entity lookup.
+ *
+ * See parent entity_lookup plugin for most config keys. Additionally, we add:
+ * - missing_behavior: One of "abort", "skip_process" or "skip_row", as per the
+ *   MissingBehaviorTrait. Defaults here to "skip_row".
  *
  * @MigrateProcessPlugin(
  *   id = "dgi_migrate.required_entity_lookup",
@@ -17,6 +23,26 @@ use Drupal\migrate_plus\Plugin\migrate\process\EntityLookup;
  */
 class RequiredEntityLookup extends EntityLookup {
 
+  use MissingBehaviorTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $pluginId, $pluginDefinition, MigrationInterface $migration = NULL) {
+    $instance = parent::create($container, $configuration, $pluginId, $pluginDefinition, $migration);
+
+    $instance->missingBehaviorInit();
+
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDefaultMissingBehavior() {
+    return 'skip_row';
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -24,7 +50,7 @@ class RequiredEntityLookup extends EntityLookup {
     $result = parent::transform($value, $migrate_executable, $row, $destination_property);
 
     if (!$result) {
-      throw new MigrateSkipRowException(strtr('Failed to find lookup entity value ":value" for property ":destination_property".', [
+      throw $this->getMissingException(strtr('Failed to find lookup entity value ":value" for property ":destination_property".', [
         ':value' => var_export($value, TRUE),
         ':destination_property' => $destination_property,
       ]));
