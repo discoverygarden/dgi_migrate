cedafa62838995a5223bb713f5aadc2b24de7fd0
Adam Vessey <adam@discoverygarden.ca>
2025-09-09 17:00:11 -0300

    Base update to plugin to avoid use of `copy()`.
    `copy()` (and more) has issues:
    - https://bugs.php.net/bug.php?id=60110
    - https://bugs.php.net/bug.php?id=53328
    - https://www.drupal.org/project/drupal/issues/3057565

--- src/Plugin/migrate/process/NaiveFileCopy.php
+++ src/Plugin/migrate/process/NaiveFileCopy.php
@@ -11,7 +11,9 @@ use Drupal\migrate\MigrateException;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Plugin\MigrateProcessInterface;
 use Drupal\migrate\Plugin\migrate\process\FileCopy;
+use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -47,11 +49,38 @@ class NaiveFileCopy extends FileCopy implements ContainerFactoryPluginInterface 
   /**
    * Constructor.
    */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition, StreamWrapperManagerInterface $stream_wrappers, FileSystemInterface $file_system, MigrateProcessInterface $download_plugin, ConfigFactoryInterface $config_factory) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $stream_wrappers, $file_system, $download_plugin);
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    array $plugin_definition,
+    StreamWrapperManagerInterface $stream_wrappers,
+    FileSystemInterface $file_system,
+    ConfigFactoryInterface $config_factory,
+    protected LoggerInterface $logger,
+  ) {
+    parent::__construct(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $stream_wrappers,
+      $file_system,
+      new class([], '', []) extends ProcessPluginBase {
+
+        /**
+         * {@inheritDoc}
+         */
+        public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) : void {
+          throw new \LogicException('Not implemented!');
+        }
+
+      },
+    );
 
     $this->migrateConfig = $config_factory->get('dgi_migrate.settings');
     $this->forceStub = $this->configuration['force_stub'] ?? FALSE;
+    if ($this->configuration['move']) {
+      throw new \LogicException("Moving files is not supported with {$this->getPluginId()}.");
+    }
   }
 
   /**
@@ -64,8 +93,8 @@ class NaiveFileCopy extends FileCopy implements ContainerFactoryPluginInterface 
       $plugin_definition,
       $container->get('stream_wrapper_manager'),
       $container->get('file_system'),
-      $container->get('plugin.manager.migrate.process')->createInstance('download', $configuration),
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('logger.factory')->get('dgi_migrate.naive_file_copy'),
     );
   }
 
@@ -91,11 +120,7 @@ class NaiveFileCopy extends FileCopy implements ContainerFactoryPluginInterface 
       }
     }
 
-    $final_destination = $this->writeFile($source, $destination, $this->configuration['file_exists']);
-    if ($final_destination) {
-      return $final_destination;
-    }
-    throw new MigrateException("File $source could not be copied to $destination");
+    return $this->writeFile($source, $destination, $this->configuration['file_exists']);
   }
 
   /**
@@ -127,57 +152,109 @@ class NaiveFileCopy extends FileCopy implements ContainerFactoryPluginInterface 
       // of "php://filter", that it returns as per the wrapped stream does not
       // appear to be correct... the usual $this->fileSystem->move/copy calls
       // would call file_exists() on the source, so let's avoid it.
-      if ($this->configuration['move']) {
-        $result = rename($source, $actual_destination);
-      }
-      else {
-        $spool_name = tempnam($this->migrateConfig->get('temp_dir'), 'b64spool');
-        try {
-          $spool_fp = fopen($spool_name, 'r+b');
-
-          if (strpos($source, 'php://filter') === 0) {
-            $target = '/resource=';
-            $pos = strpos($source, $target);
-            $actual_source = substr($source, $pos + strlen($target));
-            $source_fp = fopen($actual_source, 'rb');
-            $pipes = [];
-            $proc = proc_open($this->getBase64Command(), [
-              0 => ['pipe', 'r'],
-              1 => $spool_fp,
-            ], $pipes);
-            stream_copy_to_stream($source_fp, $pipes[0]);
-
-            fclose($pipes[0]);
-            proc_close($proc);
-            fclose($source_fp);
-          }
-          else {
-            $source_fp = fopen($source, 'rb');
-            if (!$source_fp) {
-              throw new FileException("Failed to open source.");
-            }
-            stream_copy_to_stream($source_fp, $spool_fp);
-          }
-          fflush($spool_fp);
-
-          $result = copy($spool_name, $actual_destination);
+      $spool_name = @tempnam($this->migrateConfig->get('spool_dir') ?: $this->fileSystem->getTempDirectory(), 'b64spool');
+      try {
+        $spool_fp = fopen($spool_name, 'r+b');
+        if (!$spool_fp) {
+          throw new FileException("Failed to open spool.");
         }
-        finally {
-          if (isset($spool_fp)) {
-            fclose($spool_fp);
+
+        if (str_starts_with($source, 'php://filter')) {
+          $target = '/resource=';
+          $pos = strpos($source, $target);
+          $actual_source = substr($source, $pos + strlen($target));
+          $source_fp = fopen($actual_source, 'rb');
+          if (!$source_fp) {
+            throw new FileException("Failed to open source: {$source}");
           }
-          unlink($spool_name);
+          if (!($source_stat = fstat($source_fp))) {
+            $this->logger->warning('Failed to stat source file ({source}, {actual_source}); continuing without comparing sizes.', [
+              'source' => $source,
+              'actual_source' => $actual_source,
+            ]);
+          }
+
+          $pipes = [];
+          $proc = proc_open($this->getBase64Command(), [
+            0 => ['pipe', 'r'],
+            1 => $spool_fp,
+          ], $pipes);
+          $written_to_filter = stream_copy_to_stream($source_fp, $pipes[0]);
+
+          if ($source_stat && $source_stat['size'] !== $written_to_filter) {
+            throw new FileException("Failed to write all bytes to filter: {$written_to_filter}/{$source_stat['size']} of {$source}");
+          }
+          if (!fflush($pipes[0])) {
+            throw new FileException("Failed to flush pipe for {$source}");
+          }
+          if (!fclose($pipes[0])) {
+            throw new FileException("Failed to close pipe for {$source}");
+          }
+          if (($exit_code = proc_close($proc)) !== 0) {
+            throw new FileException("Unexpected exit code for {$source}, got {$exit_code}");
+          }
+          if (!fclose($source_fp)) {
+            throw new FileException("Failed to close source file pointer, for {$source}");
+          }
         }
-      }
-      if ($result === FALSE) {
-        throw new FileException("Failed to move {$source} to {$destination} (actual attempted destination: '$actual_destination'.");
-      }
-      else {
+        else {
+          $source_fp = @fopen($source, 'rb');
+          if (!$source_fp) {
+            throw new FileException("Failed to open source.");
+          }
+          if (!($source_stat = fstat($source_fp))) {
+            $this->logger->warning('Failed to stat source file ({source}, {actual_source}); continuing without comparing sizes.', [
+              'source' => $source,
+            ]);
+          }
+          $written_to_spool = stream_copy_to_stream($source_fp, $spool_fp);
+          if ($source_stat && $source_stat['size'] !== $written_to_spool) {
+            throw new FileException("Failed to write all bytes to spool: {$written_to_spool}/{$source_stat['size']} of {$source}");
+          }
+        }
+        if (!fflush($spool_fp)) {
+          throw new FileException("Failed to flush spool.");
+        }
+
+        if (fseek($spool_fp, 0, SEEK_SET) !== 0) {
+          throw new FileException("Failed to rewind spool.");
+        }
+        clearstatcache(filename: $spool_name);
+        $spool_stat = fstat($spool_fp);
+        $destination_fp = fopen($actual_destination, 'w+b');
+        if (!$destination_fp) {
+          throw new FileException("Failed to open destination file {$destination} (actual destination: {$actual_destination}).");
+        }
+        $written_to_destination = stream_copy_to_stream($spool_fp, $destination_fp);
+        if ($spool_stat['size'] !== $written_to_destination) {
+          throw new FileException("Failed to write all bytes to destination: {$written_to_destination}/{$spool_stat['size']} , {$destination} (actual destination: {$actual_destination})");
+        }
+        if (!fflush($destination_fp)) {
+          throw new FileException("Failed to flush {$source} to {$destination} (actual destination: {$actual_destination})");
+        }
+        if (!fclose($destination_fp)) {
+          throw new FileException("Failed to close destination {$destination} (actual destination: {$actual_destination})");
+        }
+        if (!fclose($source_fp)) {
+          throw new FileException("Failed to close source {$source}");
+        }
         return $actual_destination;
+      }
+      finally {
+        if (isset($proc)) {
+          $status = @proc_get_status($proc);
+          if ($status['running']) {
+            @proc_terminate($proc, 9);
+            @proc_close($proc);
+          }
+          unset($proc);
+        }
+
+        @unlink($spool_name);
       }
     }
     catch (FileException $e) {
-      return FALSE;
+      throw new MigrateException("File $source could not be copied to $destination", previous: $e);
     }
   }
 
